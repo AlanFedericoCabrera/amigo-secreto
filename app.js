9f76b86588d0052427bb56c76a17e3bf3b22605b
Alan Cabrera <federicocabrera2002@gmail.com>
2025-08-04 13:16:46 -0300

    Crea funcion asignarTexto y agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let listaAmigosAgregar = [];
+
+function asignarTexto (elemento, texto){
+    let elementHTML = document.querySelector(elemento);
+    elementHTML.innerHTML = texto;
+    return
+}
+
+
+function agregarAmigo() {
+    let amigosDeUsuario = document.getElementById("amigo").value;    
+    listaAmigosAgregar.push(amigosDeUsuario);
+
+    let texto = "";
+    for (let i = 0; i < listaAmigosAgregar.length; i++) {
+
+        if (listaAmigosAgregar[i] == "") {
+            // Si el campo esta vacio...
+            alert("Por favor, inserte un nombre...");
+            listaAmigosAgregar.pop("");
+        }
+        else {
+            // Si ingresa un nombre: Se lo coloca uno debajo del otro.
+            texto += listaAmigosAgregar[i] + "<br>"; // Realiza un salto de linea.
+        }
+    }
+    console.log(listaAmigosAgregar);
+    document.getElementById("amigo").value = "";
+    return asignarTexto("#listaAmigos", texto);
+}
